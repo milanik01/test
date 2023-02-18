e8d8489ec08bf18af778db479ea407e08dfdb376
Liydmila <mila.ml@mail.ru>
2023-02-18 15:09:00 +0300

    "homework"

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,12 +1,40 @@
-﻿Console.Write ("Введите имя пользователя: "); 
-string username = Console.ReadLine(); 
-if(username.ToLower() == "Маша")
-{
-Console.WriteLine("Ура, это же МАША");
+﻿/*int num1 = 15;
+int num2 = 6;
+//double num2=10.5;
+//Console.Write("My numbers are " + num1 + " and "+ num2+" and it is good");
+//Console.WriteLine($"My numbers are {num1} and {num2} and it is good");
+Console.Write("Input intwger number ");
+int num = Convert.ToInt32(Console.ReadLine());
+//string number = Console.ReadLine();
+Console.Write("My number is "+ num);*/
 
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*Console.Write("Input first integer number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input seond integer number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int num3 = num2 * num2;
+
+if(num1==num3)
+{
+    Console.Write("Yes");
 }
 else
 {
-Console.Write("Привет, ");
-Console.WriteLine(username); 
+    Console.Write("No");
+}*/
+/*
+Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N.
+*/
+/*
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while(count<=num)
+{
+    Console.Write(count+" ");
+    count++;
 }
+*/
